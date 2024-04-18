a909ae1ca46931dea2340c8126b95219f7126ac2
ralenaNA <ralena@nailalliance.com>
2024-04-18 11:23:38 -0500

    Extend block and attribute assignment capacity (see explanation)
    1. Extend WebComponents block to include category id, store id, and base url grabbing functionality
    2. Extend eb-wc.phtml to handle collection-color-swatch-sampler

--- Block/WebComponents.php
+++ Block/WebComponents.php
@@ -1,8 +1,26 @@
 <?php
 namespace Nailalliance\WebComponents\Block;
 
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\Layer\Resolver as CatalogLayerResolver;
+
 class WebComponents extends \Nailalliance\EcomSwitch\Block\IpDetector
 {
+    protected $storeManager;
+    protected $catalogLayer;
+
+    public function __construct(
+        Template\Context $context,
+        StoreManagerInterface $storeManager,
+        CatalogLayerResolver $catalogLayerResolver,
+        array $data = []
+    ) {
+        $this->storeManager = $storeManager;
+        $this->catalogLayer = $catalogLayerResolver->get();
+        parent::__construct($context, $data);
+    }
+
     public function checkAllowed()
     {
         return $this->isAllowed();
@@ -12,4 +30,27 @@ class WebComponents extends \Nailalliance\EcomSwitch\Block\IpDetector
     {
         return $this->getData('txtfiles');
     }
+
+    public function getCurrentStoreId()
+    {
+        return $this->storeManager->getStore()->getId();
+    }
+
+    public function getCurrentBaseUrl()
+    {
+        return $this->storeManager->getStore()->getBaseUrl();
+    }
+
+    public function getCurrentCategoryId()
+    {
+        try {
+            if ($category = $this->catalogLayer->getCurrentCategory()) {
+                return $category->getId();
+            }
+        } catch (\Exception $e) {
+            return null;  // Handle the situation when the category is not available
+        }
+        return null;
+    }
 }
+?>
